8c08a59ea59c1f87f419d129871759d2ba5cfdec
chengwuyouxin <liupengqiang0823@126.com>
2018-02-26 17:51:20 +0800

    submit sql

--- src/personallibrary.sql
+++ src/personallibrary.sql
@@ -1,5 +1,4 @@
--- MySQL dump 10.13  Distrib 5.7.17, for Win64 (x86_64)
---
+﻿-- MySQL dump 10.13  Distrib 5.7.17, for Win64 (x86_64)
 -- Host: localhost    Database: personallibrary
 -- ------------------------------------------------------
 -- Server version	5.7.17
